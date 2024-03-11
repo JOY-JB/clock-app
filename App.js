a09ceb409dd721495552820688d791144739a4f8
JOY-JB <joybarua0058@gmail.com>
2024-03-12 03:41:41 +0600

    app initialized and some preview added

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Inter-Regular": Inter_400Regular,
+    "Inter-Bold": Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/bg.jpg")}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.root}>
+        <View style={{ flexDirection: "row" }}>
+          <View>
+            <Text style={styles.quote}>
+              "The advance of technology is based on making it fit in so that
+              you don't really even notice it, so it's part of everyday life."
+            </Text>
+            <Text style={styles.author}> - Bill Gates</Text>
+          </View>
+        </View>
+      </SafeAreaView>
+
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  root: {
+    flex: 1,
+    justifyContent: "space-between",
+    marginTop: 32,
+    paddingHorizontal: 16,
+  },
+  quote: {
+    fontFamily: "Inter-Regular",
+    fontSize: 14,
+    color: "white",
+  },
+  author: {
+    fontFamily: "Inter-Bold",
+    fontSize: 14,
+    color: "white",
+    marginTop: 8,
   },
 });
